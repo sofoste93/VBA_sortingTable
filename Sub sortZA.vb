0812ca2fd1d7a4097ec6097e60510385e0d9f239
Stephane Sob Fouodji <stefan-kruger7@hotmail.com>
2021-05-26 14:07:04 +0200

    Create Sub sortZA.vb
    Co-Authored-By: Stephane Sob Fouodji <stefan-kruger7@hotmail.com>

---
+++ Sub sortZA.vb
@@ -0,0 +1,18 @@
+Sub sortZA()
+'
+' sortZA Macro
+'
+
+'
+    ActiveWorkbook.Worksheets("Sheet1").AutoFilter.Sort.SortFields.Clear
+    ActiveWorkbook.Worksheets("Sheet1").AutoFilter.Sort.SortFields.Add2 Key:= _
+        Range("A1"), SortOn:=xlSortOnValues, Order:=xlDescending, DataOption:= _
+        xlSortNormal
+    With ActiveWorkbook.Worksheets("Sheet1").AutoFilter.Sort
+        .Header = xlYes
+        .MatchCase = False
+        .Orientation = xlTopToBottom
+        .SortMethod = xlPinYin
+        .Apply
+    End With
+End Sub
